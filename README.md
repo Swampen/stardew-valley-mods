51578ad11aa1ffc3479527a2246a338acf822bda
Swampen <ole-ma@outlook.com>
2021-11-07 10:03:38 +0100

    Restructured project to have multiple mods

---
+++ README.md
@@ -0,0 +1,20 @@
+# Stardew Valley Mods
+This repository contain the Stardew Valley mods I have made.
+
+## Install
+1. [Install the latest version of SMAPI](https://smapi.io)
+2. Download mods and unzip them into *Stardew Valley/Mods*.
+3. Run the game using StardewModdingAPI (SMAPI).
+
+## [QueenOfSauceReminder](https://www.nexusmods.com/stardewvalley/mods/5172/)
+
+Reminds you in the morning when the Queen of Sauce program is airing on TV.
+I made this mod because I always forgot to watch it.
+
+## Compiling
+To compile a SMAPI mod for testing, use the following instructions:
+
+1. Make sure to have the appropriate versions of the game and [SMAPI](https://smapi.io) installed.
+2. Open the solution with Visual Studio or MonoDevelop.
+3. Add the package [Stardew.ModBuildConfig](https://www.nuget.org/packages/Pathoschild.Stardew.ModBuildConfig) by Pathoschild to enable cross-platform compatibility.
+4. Re-build the solution and run the debugger to launch the project with SMAPI.
---
+++ TheQueenOfSauceReminder/ModEntry.cs
@@ -0,0 +1,36 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace TheQueenOfSauceReminder
+{
+    public class ModEntry : Mod
+    {
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.GameLoop.DayStarted += this.DayStarted;
+        }
+
+        /*********
+        ** Private methods
+        *********/
+        /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
+        private void DayStarted(object sender, DayStartedEventArgs e)
+        {
+            SDate sdate1 = SDate.Now();
+            SDate sdate2 = new SDate(3, "spring");
+            SDate sdate3 = new SDate(7, "spring");
+            if (sdate1.DayOfWeek != sdate2.DayOfWeek && sdate1.DayOfWeek != sdate3.DayOfWeek)
+                return;
+            Game1.drawObjectDialogue("Reminder: The Queen of Sauce is airing on TV today");
+        }
+    }
+}
