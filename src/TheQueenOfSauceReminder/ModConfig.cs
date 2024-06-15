0a4754bb91b61e6105a3cc857dbb6cf46a06cc88
Swampen <ole-ma@outlook.com>
2024-06-15 23:28:34 +0200

    Add config for unknown only

--- src/TheQueenOfSauceReminder/ModConfig.cs
+++ src/TheQueenOfSauceReminder/ModConfig.cs
@@ -4,4 +4,6 @@ public class ModConfig
 {
     public bool EnableWednesdayReminder { get; set; } = true;
     public bool EnableSundayReminder { get; set; } = true;
+
+    public bool RemindOnlyUnknownRecipes { get; set; } = false;
 }
